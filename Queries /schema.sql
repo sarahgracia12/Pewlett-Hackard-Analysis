a5b958d18256e0d6ab333ff42d8471076c2c2d23
sarahgracia12 <71557028+sarahgracia12@users.noreply.github.com>
2020-11-10 19:29:35 -0500

    queries

--- Queries /schema.sql
+++ Queries /schema.sql
@@ -37,18 +37,21 @@ CREATE TABLE salaries (
 --Creating tables for PH-EmployeeDB
 CREATE TABLE titles(
 	emp_no INT NOT NULL,
-	title INT NOT NULL,
+	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (title)
+	PRIMARY KEY (title, emp_no, from_date)
 );
 --Create tables for PH-employeeDB
 CREATE TABLE dept_emp (
-	dept_no VARCHAR(4) NOT NULL,
+	dept_no VARCHAR NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no, dept_no)
+    PRIMARY KEY (dept_no),
+	UNIQUE (emp_no)
+	
+);
